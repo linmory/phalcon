d2fcbe1076d7f6facad1dc84e404622f3051e1a1
AlloVince <allo.vince@gmail.com>
2014-06-17 11:00:49 +0800

    Media filters

--- modules/EvaFileSystem/src/EvaFileSystem/Controllers/Admin/MediaController.php
+++ modules/EvaFileSystem/src/EvaFileSystem/Controllers/Admin/MediaController.php
@@ -3,6 +3,7 @@
 namespace Eva\EvaFileSystem\Controllers\Admin;
 
 use Eva\EvaFileSystem\Models;
+use Eva\EvaFileSystem\Forms;
 
 class MediaController extends ControllerBase
 {
@@ -15,22 +16,33 @@ class MediaController extends ControllerBase
      */
      public function indexAction()
      {
-        $currentPage = $this->request->getQuery('page', 'int'); // GET
-        $limit = $this->request->getQuery('limit', 'int');
-        $limit = $limit > 50 ? 50 : $limit;
+        $limit = $this->request->getQuery('limit', 'int', 25);
+        $limit = $limit > 100 ? 100 : $limit;
         $limit = $limit < 10 ? 10 : $limit;
-
-        $posts = $this->modelsManager->createBuilder()
-            ->from('Eva\EvaFileSystem\Models\FileManager')
-            ->orderBy('id DESC');
-        $paginator = new \Eva\EvaEngine\Paginator(array(
-            "builder" => $posts,
-            "limit"=> $limit,
-            "page" => $currentPage
-        ));
-        $paginator->setQuery(array(
+        $order = $this->request->getQuery('order', 'string', '-created_at');
+        $query = array(
+            'q' => $this->request->getQuery('q', 'string'),
+            'status' => $this->request->getQuery('status', 'string'),
+            'uid' => $this->request->getQuery('uid', 'int'),
+            'extension' => $this->request->getQuery('extension', 'string'),
+            'image' => $this->request->getQuery('image', 'int'),
+            'order' => $order,
             'limit' => $limit,
+            'page' => $this->request->getQuery('page', 'int', 1),
+        );
+
+        $form = new Forms\FilterForm();
+        $form->setValues($this->request->getQuery());
+        $this->view->setVar('form', $form);
+
+        $fileManager = new Models\FileManager();
+        $medias = $fileManager->findFiles($query);
+        $paginator = new \Eva\EvaEngine\Paginator(array(
+            "builder" => $medias,
+            "limit"=> $limit,
+            "page" => $query['page']
         ));
+        $paginator->setQuery($query);
         $pager = $paginator->getPaginate();
         $this->view->setVar('pager', $pager);
     }
