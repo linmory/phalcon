a8377497174ed334a3d28a22ab0db12bed6d0b0b
AlloVince <allo.vince@gmail.com>
2014-06-19 17:04:46 +0800

    add resource scanner

---
+++ modules/EvaPermission/src/EvaPermission/Utils/Scanner.php
@@ -0,0 +1,182 @@
+<?php
+/**
+* EvaEngine (http://evaengine.com/)
+*
+* @copyright Copyright (c) 2014 AlloVince (allo.vince@gmail.com)
+* @license   http://framework.zend.com/license/new-bsd New BSD License
+*/
+
+namespace Eva\EvaPermission\Utils;
+
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+class Scanner
+{
+    protected $scanPath;
+
+    public function scan()
+    {
+        $scanPath = $this->scanPath;
+        $finder = new Finder();
+        $iterator = $finder
+        ->files()
+        ->name('*Controller.php')
+        ->in($scanPath)
+        ->exclude('vendor');
+        $this->controllers = $iterator;
+        return $this;
+    }
+
+    public function getControllers()
+    {
+        return $this->controllers;
+    }
+
+    public function process(SplFileInfo $controller)
+    {
+        $controllerPath = $controller->getPathName();
+        $controllerClass = $this->getClassName($controller->getContents());
+        if(!$controllerClass) {
+            return $this;
+        }
+
+        $resource = $this->getResource($controllerClass);
+
+        if(!$resource) {
+            return $this;
+        }
+
+        $operations = $this->getOperations($controllerClass);
+        return $this;
+    }
+
+    protected function getClassName($sourceCode)
+    {
+        $tokens = token_get_all($sourceCode);
+        $tokenLength = count($tokens);
+        for($i = 0; $i < $tokenLength; $i++) {
+
+            if(isset($tokens[$i][1]) && strtolower($tokens[$i][1]) == 'namespace') {
+                $j = 1;
+                $namespace = '';
+                while(true) {
+                    if(empty($tokens[$i + $j][1])) {
+                        if($j != 1) {
+                            break;
+                        }
+                    }
+                    $namespace .= $tokens[$i + $j][1];
+                    $j++;
+                }
+                $namespaces[] = trim($namespace); //there can be only one (=> highlander) per file, but by not assuming we could find errors/throw exceptions
+                $i += $j;
+            }
+
+            if(isset($tokens[$i][1]) && strtolower($tokens[$i][1]) == 'class') {
+                $j = 1;
+                $class = '';
+                while(true) {
+                    if(isset($tokens[$i + $j][1]) && trim($tokens[$i + $j][1]) == '') {
+                        if($j != 1) {
+                            break;
+                        }
+                    }
+                    $class .= $tokens[$i + $j][1];
+                    $j++;
+                }
+                $classes[] = trim($class); //there can be only one (=> highlander) per file, but by not assuming we could find errors/throw exceptions
+                $i += $j;
+            }
+        }
+
+        if(empty($namespaces) || empty($classes)) {
+            return '';
+        }
+        return $namespaces[0] . '\\' . $classes[0];
+    }
+
+    public function getResource($controllerClass)
+    {
+        $ref = new \ReflectionClass($controllerClass);
+        //Not a private resource
+        if(!$ref->implementsInterface('Eva\EvaEngine\Mvc\Controller\TokenAuthorityControllerInterface') &&
+          !$ref->implementsInterface('Eva\EvaEngine\Mvc\Controller\SessionAuthorityControllerInterface')) {
+            return false;
+        }
+        
+        $resourceGroup = 'app';
+        if($ref->implementsInterface('Eva\EvaEngine\Mvc\Controller\TokenAuthorityControllerInterface')) {
+            $resourceGroup = 'api';
+        } else {
+            if($ref->isSubclassOf('Eva\EvaEngine\Mvc\Controller\AdminControllerBase')) {
+                $resourceGroup = 'admin';
+            }
+        }
+
+        $reader = new \Phalcon\Annotations\Adapter\Memory();
+        $reflector = $reader->get($controllerClass);
+        $resourceAnnotations = $reflector->getClassAnnotations();
+        $resourceName = $controllerClass;
+        $resourceDes = '';
+
+        if($resourceAnnotations && $annotation = $resourceAnnotations->get('resourceName')) {
+            $resourceName = implode('', $annotation->getArguments());
+        }
+
+        if($resourceAnnotations && $annotation = $resourceAnnotations->get('resourceDescription')) {
+            $resourceDes = implode('', $annotation->getArguments());
+        }
+
+        $resource = array(
+            'name' => $resourceName,
+            'resourceKey' => $controllerClass,
+            'resourceGroup' => $resourceGroup,
+            'description' => $resourceDes,
+        );
+
+        return $resource;
+    }
+
+    public function getOperations($controllerClass)
+    {
+        $operations = array();
+
+        $ref = new \ReflectionClass($controllerClass);
+        $methods = $ref->getMethods();
+        $reader = new \Phalcon\Annotations\Adapter\Memory();
+        $reflector = $reader->get($controllerClass);
+        $operationAnnotations = $reflector->getMethodsAnnotations();
+
+        foreach($methods as $method) {
+            if(!\Phalcon\Text::endsWith($method->name, 'Action')) {
+                continue;
+            }
+
+            $operationKey = substr($method->name, 0, -6);
+            $operationName = $operationKey;
+            $operationDes = '';
+
+            if(isset($operationAnnotations[$method->name]) && $annotations = $operationAnnotations[$method->name]) {
+                $annotation = $annotations->get('operationName');
+                $operationName = implode('', $annotation->getArguments());
+                $annotation = $annotations->get('operationDescription');
+                $operationDes = implode('', $annotation->getArguments());
+            }
+
+            $operation = array(
+                'name' => $operationName,
+                'resourceKey' => $controllerClass,
+                'operationKey' => $operationKey,
+                'description' => $operationDes,
+            );
+            $operations[] = $operation;
+        }
+        return $operations;
+    }
+
+    public function __construct($scanPath = __DIR__)
+    {
+        $this->scanPath = $scanPath;
+    }
+}
